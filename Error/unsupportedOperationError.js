91e490e7cd0fc2e5cf16cbeeac00795244d9a9ed
chiru7 <nagato3c+github@gmail.com>
2017-03-13 07:54:03 +0900

    add Error class

---
+++ Error/unsupportedOperationError.js
@@ -0,0 +1,11 @@
+class UnsupportedOperationError extends Error {
+  constructor(...args) {
+    super(...args);
+    Object.defineProperty(this, 'name', {
+      value: this.constructor.name,
+    });
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = UnsupportedOperationError;
